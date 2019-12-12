8d2926eae0b00ada343c7165996b74faef7b58e7
Qiu-Jiaqi <qiu_jiaqi@qq.com>
2019-12-12 13:04:58 +0800

    2019-12-12 206. 反转链表

---
+++ linked-list/206.反转链表.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
