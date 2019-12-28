2059563c2f5aa505bc7128c3abfad85460c8daa9
Qiu-Jiaqi <qiu_jiaqi@qq.com>
2019-12-28 13:55:00 +0800

    2019-12-28 101. 对称二叉树

---
+++ binary-tree/101.对称二叉树.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root, root);
+    }
+
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        return left.val == right.val && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
